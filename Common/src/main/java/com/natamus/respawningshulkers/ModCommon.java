57bd1164b5cf8318348033f1ab038d61c779a6f8
Rick South <rick@natamus.com>
2023-12-12 00:14:43 +0100

    Respawning Shulkers source code for 1.20.4.

---
+++ Common/src/main/java/com/natamus/respawningshulkers/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.respawningshulkers;
+
+import com.natamus.respawningshulkers.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
