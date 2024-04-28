8fd26285f05ad77cb4551eb7fdd01ee5050b16a4
Rick South <rick@natamus.com>
2024-04-28 18:58:08 +0200

    (v3.8) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/respawningshulkers/neoforge/events/NeoForgeShulkerEvent.java
+++ NeoForge/src/main/java/com/natamus/respawningshulkers/neoforge/events/NeoForgeShulkerEvent.java
@@ -4,8 +4,7 @@ import com.natamus.respawningshulkers.events.ShulkerEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.LevelTickEvent;
-import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.neoforged.neoforge.event.server.ServerStoppingEvent;
 import net.neoforged.bus.api.EventPriority;
@@ -15,9 +14,9 @@ import net.neoforged.fml.common.EventBusSubscriber;
 @EventBusSubscriber
 public class NeoForgeShulkerEvent {
 	@SubscribeEvent
-	public static void onWorldTick(LevelTickEvent e) {
-		Level level = e.level;
-		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+	public static void onWorldTick(LevelTickEvent.Pre e) {
+		Level level = e.getLevel();
+		if (level.isClientSide) {
 			return;
 		}
 		
