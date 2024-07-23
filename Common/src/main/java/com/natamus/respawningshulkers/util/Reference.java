9126a19e4ac1750bde220ce8f84578d662cf1d7c
Rick South <rick@natamus.com>
2024-07-23 12:25:57 +0200

    (v3.9) Added compatibility with the Flower Mimics mod, so shulker mimics don't respawn.

--- Common/src/main/java/com/natamus/respawningshulkers/util/Reference.java
+++ Common/src/main/java/com/natamus/respawningshulkers/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.respawningshulkers.util;
 public class Reference {
 	public static final String MOD_ID = "respawningshulkers";
 	public static final String NAME = "Respawning Shulkers";
-	public static final String VERSION = "3.8";
+	public static final String VERSION = "3.9";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
