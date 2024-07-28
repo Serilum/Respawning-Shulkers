56b568530df3e659e5f6b9f8e19389382b0d06b2
Rick South <rick@natamus.com>
2024-07-28 07:16:45 +0200

    (v4.0) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/respawningshulkers/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/respawningshulkers/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.respawningshulkers;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.respawningshulkers.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.respawningshulkers.neoforge.events.NeoForgeShulkerEvent;
 import com.natamus.respawningshulkers.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
