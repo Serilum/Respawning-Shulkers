c17f6d857b452fe6a46ab65e0282d39815989499
Rick South <rick@natamus.com>
2025-02-12 23:15:51 +0100

    (v4.1) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Common/src/main/java/com/natamus/respawningshulkers/util/Reference.java
+++ Common/src/main/java/com/natamus/respawningshulkers/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.respawningshulkers.util;
 public class Reference {
 	public static final String MOD_ID = "respawningshulkers";
 	public static final String NAME = "Respawning Shulkers";
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "4.1";
 	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
 }
